b133e66cb59dd7d6934093c3ea613e36556ff635
Ashish Kumar <87820654+heyashishsaini@users.noreply.github.com>
2026-04-13 22:18:07 +0530

    Update GitHub link in Footer component

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
     <hr />
     <p style={{textAlign: 'center', fontSize: isMobile ? '10px' : '15px'}}>Follow Playlist on <Link to="https://open.spotify.com/playlist/24QyAedxzc1U0bxV6eFoSX?si=ced57226d9f145c6">Spotify</Link></p>
 
-    <p style={{textAlign: 'center', fontSize: isMobile ? '10px' : '15px'}}>© desiRhyme 2025 | All Rights Reserved. <Link to="https://github.com/ashishalf/desirhyme/blob/main/README.md">Github</Link></p>
+    <p style={{textAlign: 'center', fontSize: isMobile ? '10px' : '15px'}}>© desiRhyme 2025 | All Rights Reserved. <Link to="https://github.com/heyashishsaini/desirhyme/blob/main/README.md">Github</Link></p>
     </>
   )
 }
